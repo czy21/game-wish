10d744ac70a10609cf99353b9da4ed7443e3834c
Bruce Chen <805899926@qq.com>
2025-03-11 11:29:53 +0800

    # add server

---
+++ Server/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+
+namespace Server.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,15 @@
+using sunny_game_server_csharp.Services;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
