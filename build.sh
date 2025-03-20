b89ae3bbd6bae11dd8e5ecd4a71bb7d2db50171f
Bruce Chen <805899926@qq.com>
2025-03-20 16:13:34 +0800

    # adjust nlog.config

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ cd $(cd "$(dirname "$0")"; pwd)
 
 rm -rf Server/build
 dotnet publish -c Release Server -r linux-x64 --self-contained false -p:PublishSingleFile=true -p:AssemblyName=api -o Server/build
-rm -rf Server/build/appsettings*.json
+find Server/build -name "appsettings*.json" -and -not -name "appsettings.json" -delete
 
 tar zcvf $(pwd)/api.tar.gz -C $(pwd)/Server/build/ .
